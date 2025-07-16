1215e7b02ac5e10c7121a32b5d5bb415be24dab9
csl <songsong8040@gmail.com>
2025-07-16 15:34:14 +0900

    추가

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Button1 from "./components/Button1.jsx";
+import Button2 from "./components/Button2.jsx";
+import StyleComponent from "./StyleComponent.jsx";
+import TailwindComponent from "./TailwindComponent.jsx";
+
+function App() {
+  return (
+    <>
+        <Button1/>
+        <Button2/>
+        <StyleComponent/>
+        <TailwindComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,8 @@
+export default function TailwindComponent() {
+  return (
+    <>
+        <h1 className="text-8xl">안녕</h1>
+        <button className="bg-blue-400 rounded-xl px-2">Button</button>
+    </>
+  )
+}
